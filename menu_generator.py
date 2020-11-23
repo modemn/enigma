59ccb1e483a9ff65139f84c4fe8ec0ad1a6a6b4f
Nikhil Modem <Nikhil@nikhils-mbp.home>
2020-11-23 16:23:58 +0000

    Made my own menu thing, takes the plain and cipher of the crib and generates the graph. Makes looking at closures easier

---
+++ menu_generator.py
@@ -0,0 +1,41 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# plain_crib = input()
+# cipher_crib = input()
+
+plain_crib = 'ANATTACKONDURHAMAT'
+cipher_crib = 'ZPJABYGHRHGYZJILRJ'
+
+letters = plain_crib+cipher_crib
+# letters = "".join(set(letters))
+
+menu = nx.Graph()
+
+menu.add_nodes_from(letters)
+
+connections = tuple(zip(plain_crib, cipher_crib))
+
+edge_labels = {}
+for i in range(len(connections)):
+    edge_labels[connections[i]] = str(i+1)
+
+menu.add_edges_from(connections)
+
+pos = nx.planar_layout(menu)
+
+plt.figure()
+
+nx.draw(menu, pos, edge_color='black', width=1, linewidths=1,
+        node_size=200, node_color='red', alpha=0.9,
+        labels={node: node for node in menu.nodes()})
+
+nx.draw_networkx_edge_labels(
+    menu, pos, edge_labels=edge_labels, font_color='black')
+
+plt.axis('off')
+plt.show()
+# plt.savefig('menu.png')
+
+# nx.draw(menu, with_labels=True, font_weight='bold')
+# plt.show()
