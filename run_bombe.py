714e675cf5fe4a1198c041a09b6a27213a51c5f6
Nikhil Modem <Nikhil@nikhils-mbp.lan>
2021-01-30 16:02:48 +0000

    Added a file to run the Bombe from

---
+++ run_bombe.py
@@ -0,0 +1,16 @@
+from bombe_2 import Bombe
+
+b = Bombe(
+    'II',  # TOP / LEFT ROTOR
+    'V',  # MIDDLE ROTOR
+    'III',  # BOTTOM / RIGHT ROTOR
+    'ZZZ',
+    'B',  # REFLECTOR
+    ['ZZK', 'ZZE', 'ZZF', 'ZZN', 'ZZM', 'ZZG',
+        'ZZP', 'ZZB', 'ZZJ', 'ZZI', 'ZZL', 'ZZO'],  # SCRAMBLER SETTINGS
+    ['UE', 'EG', 'GR', 'RA', 'AS', 'SV', 'VE', 'EN',
+        'HZ', 'ZR', 'RG', 'GL'],  # CONNECTIONS
+    'E'  # INPUT LETTER
+)
+
+b.run()
