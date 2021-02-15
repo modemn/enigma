b7350033f0be43a5fd00b0037c391b56ed4965c5
Nikhil Modem <Nikhil@nikhils-mbp.home>
2021-02-15 10:36:36 +0000

    Moved the continuous function to its own file

--- bombe_UI.py
+++ bombe_UI.py
@@ -2,6 +2,7 @@ from timer import Timer
 from bombe_2_for_UI import Bombe
 import PySimpleGUI as sg
 from menu_generator_1 import MenuGenerator
+import run_continuous
 
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -167,38 +168,20 @@ while True:  # Event Loop
         if values['continuous']:
             ROTORS = ['I', 'II', 'III', 'IV', 'V']
             REFLECTORS = ['A', 'B', 'C']
-            rotor_combos = [(x, y, z)
-                            for x in ROTORS for y in ROTORS for z in ROTORS if x != y if y != z if x != z]
 
             timer = Timer()
             timer.start()
-            i = 0
-            for top_rotor, middle_rotor, bottom_rotor in rotor_combos:
-                for reflector in REFLECTORS:
-                    i += 1
-                    print(f'Settings {i}/180')
-                    print('******************BOMBE******************')
-                    print('Running the Bombe with the following settings:')
-                    print('Rotors:', top_rotor, middle_rotor, bottom_rotor)
-                    print('Reflector:', reflector)
-                    print('Starting Letters:', starting_letters)
-                    print('Input Letter:', input_letter)
-                    print('*****************************************')
-                    print()
 
-                    b = Bombe(
-                        top_rotor,
-                        middle_rotor,
-                        bottom_rotor,
-                        starting_letters,
-                        reflector,
-                        settings,
-                        connections,
-                        input_letter
-                    )
-
-                    print('RUNNING...')
-                    b.auto_run(plain_crib, cipher_crib)
+            run_continuous.run(
+                ROTORS,
+                REFLECTORS,
+                starting_letters,
+                input_letter,
+                settings,
+                connections,
+                plain_crib,
+                cipher_crib
+            )
 
             print('Total time through all rotors:', timer.stop())
         else:
