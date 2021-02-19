9325d708e569627596854dc8f1c5df4c722075be
Nikhil Modem <Nikhil@nikhils-mbp.home>
2021-02-19 10:37:58 +0000

    Used a queue to collect stops returned from the bombe and also changed it to only generate the settings once at the beginning and using the normal bombe class instead of running the auto bombe each time to make it quicker

--- continuous_parallel_bombe.py
+++ continuous_parallel_bombe.py
@@ -1,25 +1,67 @@
+from menu_generator_1 import MenuGenerator
+from bombe_2 import Bombe
+import multiprocessing
 import sys
 import time
-from multiprocessing import Process
-import run_auto_bombe
 
 ROTORS = ['I', 'II', 'III', 'IV', 'V']
-REFLECTORS = ['A', 'B', 'C']
+# REFLECTORS = ['A', 'B', 'C']
+REFLECTORS = ['B']
 
-plain_crib = sys.argv[1].upper()
-cipher_crib = sys.argv[2].upper()
-reflector = sys.argv[3].upper()
-starting_letters = sys.argv[4].upper()
+
+def run_bombe(
+    plain_crib,
+    cipher_crib,
+    top_rotor,
+    middle_rotor,
+    bottom_rotor,
+    starting_letters,
+    reflector,
+    settings,
+    connections,
+    input_letter,
+    printing,
+    output,
+    q
+):
+
+    b = Bombe(
+        top_rotor,
+        middle_rotor,
+        bottom_rotor,
+        starting_letters,
+        reflector,
+        settings,
+        connections,
+        input_letter,
+        printing,
+        output
+    )
+
+    res = b.auto_run(plain_crib, cipher_crib)
+
+    q.put(res)
 
 
 if __name__ == '__main__':
+    plain_crib = sys.argv[1].upper()
+    cipher_crib = sys.argv[2].upper()
+    starting_letters = sys.argv[3].upper()
+
     starttime = time.time()
     processes = []
+    q = multiprocessing.Queue()
+    rets = []
+
+    mg = MenuGenerator(plain_crib, cipher_crib, starting_letters)
+    settings, connections, input_letter, _ = mg.get_bombe_settings()
+
     rotor_combos = [
         (x, y, z) for x in ROTORS for y in ROTORS for z in ROTORS if x != y if y != z if x != z]
+
     for top_rotor, middle_rotor, bottom_rotor in rotor_combos:
         for reflector in REFLECTORS:
-            p = Process(target=run_auto_bombe.run, args=(
+            p = multiprocessing.Process(target=run_bombe, args=(
                 plain_crib,
                 cipher_crib,
                 top_rotor,
@@ -27,13 +69,24 @@ if __name__ == '__main__':
                 bottom_rotor,
                 starting_letters,
                 reflector,
+                settings,
+                connections,
+                input_letter,
                 False,
-                False
+                False,
+                q
             ))
             processes.append(p)
             p.start()
 
     for process in processes:
+        ret = q.get()
+        rets.append(ret)
+
+    for process in processes:
         process.join()
 
     print('That took {} seconds'.format(time.time() - starttime))
+    result = []
+    list(map(result.extend, rets))
+    print(result)
